e7c601342b61c884fa452b0857b972306e921318
Andy Blackwell <andy.blackwell@announcemedia.com>
2012-10-14 20:01:34 -0700

    templating partially done

--- js/site.js
+++ js/site.js
@@ -21,8 +21,8 @@ var html5dev = (function(){
 			var store = null;
 	
 			if('localStorage' in window && window['localStorage'] !== null){
-				store = window.localStorage;
-				my_events = store.get('my_events');
+				//store = window.localStorage;
+				//my_events = store.get('my_events');
 			}
 			
 			// Populate JSON object with user preferences for event attendence
@@ -30,6 +30,24 @@ var html5dev = (function(){
 			// Build HTML table
 			
 			// Live bind click events to table items, bind on single element and let bubble up with nodetype detection
+			var time_blocks = [];
+			var sessions = _.filter(html5devconf_data.schedule, function(data) { return data.day == 1; });
+			for(i in html5devconf_data.time_blocks){
+				console.log(html5devconf_data.time_blocks[i]);
+				var time_block = _.filter(sessions, function(session) { return session.time == html5devconf_data.time_blocks[i]; });
+				if( time_block ) time_blocks.push({ time:html5devconf_data.time_blocks[i], sessions:time_block});
+			}
+			var html = _.template($('#calendar-template').html(), {title:'Day 1 - Monday October 15', time_blocks: time_blocks});
+			$( html ).appendTo($('#event-list'));
+			
+			var time_blocks = [];
+			var sessions = _.filter(html5devconf_data.schedule, function(data) { return data.day == 2; });
+			for(i in html5devconf_data.time_blocks){
+				var time_block = _.filter(sessions, function(session) { return session.time == html5devconf_data.time_blocks[i]; });
+				if( time_block ) time_blocks.push({ time:html5devconf_data.time_blocks[i], sessions:time_block});
+			}
+			var html = _.template($('#calendar-template').html() , {title:'Day 2 - Monday October 16', time_blocks: time_blocks});
+			$( html ).appendTo($('#event-list'));
 			
 			html5dev.update_layout();
 		},
